d40d51abb0e55f1e53e793116167b93da990a780
Gobuub <botica1982@msn.com>
2022-10-16 21:01:43 +0200

    Upload day 2 week 2

---
+++ Apuntes Clase/Week_2/Day_2/Lab_Exercices/vikingsClasses.py
@@ -0,0 +1,36 @@
+
+# Soldier
+
+
+class Soldier:
+    
+    def __init__(self, health, strength):
+        
+        self.health = health
+        self.strength = strength
+    
+    def attack(self):
+        
+        return self.strength
+    
+    def receiveDamage(self, damage):
+        
+        self.health = self.health - damage
+
+# Viking
+
+
+class Viking:
+    pass
+
+# Saxon
+
+
+class Saxon:
+    pass
+
+# War
+
+
+class War:
+    pass
